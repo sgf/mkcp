5f0fcf1a7fa566e05eb629edf7a0204dc29fb61b
jamie.shi <jamie.shi@penseesoft.com>
2020-10-09 20:48:41 +0800

    提交

--- mkcp/mkcp/SpanEx.cs
+++ mkcp/mkcp/SpanEx.cs
@@ -7,8 +7,6 @@ using System.Text;
 namespace mkcp {
     internal static class SpanEx {
 
-
-
         public static ref T Read<T>(this Span<byte> buff) where T : struct => ref MemoryMarshal.AsRef<T>(buff);
         public static ref T Read<T>(this Span<byte> buff, int offset) where T : struct => ref MemoryMarshal.AsRef<T>(buff.Slice(offset));
 
